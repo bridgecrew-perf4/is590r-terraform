b852dcba47de15ff151e413cccff45de0476009a
Oleg Simonov <simonov.vsk@gmail.com>
2021-03-04 09:26:35 -0700

    first commit

---
+++ main.tf
@@ -0,0 +1,14 @@
+# 1.    VPC
+# 2.    Internet Gateway
+# 3.    Custom Route Table (might be optional)
+# 4.    Private Subnet for Staging ENV
+# 5.    Public Subnet for Prod ENV
+# 6.    Security Group for Staging
+# 7.    Security Group or Prod
+# 8.    Network Interface
+# 9.    Elastic IP Address
+# 10.   Ubuntu Server with Apache for the Front-end
+# 11.   ECS (container service)
+# 12.   RDS
+#   12.1 Staging DB
+#   12.2 Prod DB
